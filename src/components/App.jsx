fe992ca377bee6e29e1006897f18b833d170d6cb
Luka Avramovic <avramoviclu@gmail.com>
2023-04-10 21:50:38 +0200

    Fixes indent spaces.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,136 +5,136 @@ import TodoList from './TodoList';
 import '../App.css';
 
 function App() {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            title: 'Finish React Series',
-            isComplete: false,
-            isEditing: false,
-        },
-        {
-            id: 2,
-            title: 'Go Grocery',
-            isComplete: true,
-            isEditing: false,
-        },
-        {
-            id: 3,
-            title: 'Take over world',
-            isComplete: false,
-            isEditing: false,
-        },
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish React Series',
+      isComplete: false,
+      isEditing: false,
+    },
+    {
+      id: 2,
+      title: 'Go Grocery',
+      isComplete: true,
+      isEditing: false,
+    },
+    {
+      id: 3,
+      title: 'Take over world',
+      isComplete: false,
+      isEditing: false,
+    },
+  ]);
+
+  const [idForTodo, setIdForTodo] = useState(4);
+
+  function addTodo(todo) {
+    setTodos([
+      ...todos,
+      {
+        id: idForTodo,
+        title: todo,
+        isComplete: false,
+      },
     ]);
 
-    const [idForTodo, setIdForTodo] = useState(4);
+    setIdForTodo(prevIdForTodo => prevIdForTodo + 1);
+  }
 
-    function addTodo(todo) {
-        setTodos([
-            ...todos,
-            {
-                id: idForTodo,
-                title: todo,
-                isComplete: false,
-            },
-        ]);
+  function deleteTodo(id) {
+    setTodos([...todos].filter(todo => todo.id !== id));
+  }
 
-        setIdForTodo(prevIdForTodo => prevIdForTodo + 1);
-    }
+  function completeTodo(id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
 
-    function deleteTodo(id) {
-        setTodos([...todos].filter(todo => todo.id !== id));
-    }
+      return todo;
+    });
 
-    function completeTodo(id) {
-        const updatedTodos = todos.map(todo => {
-            if (todo.id === id) {
-                todo.isComplete = !todo.isComplete;
-            }
+    setTodos(updatedTodos);
+  }
 
-            return todo;
-        });
+  function markAsEditing(id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isEditing = true;
+      }
 
-        setTodos(updatedTodos);
-    }
+      return todo;
+    });
 
-    function markAsEditing(id) {
-        const updatedTodos = todos.map(todo => {
-            if (todo.id === id) {
-                todo.isEditing = true;
-            }
+    setTodos(updatedTodos);
+  }
 
-            return todo;
-        });
+  function updateTodo(event, id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        if (event.target.value.trim().length === 0) {
+          todo.isEditing = false;
+          return todo;
+        }
+        todo.title = event.target.value;
+        todo.isEditing = false;
+      }
 
-        setTodos(updatedTodos);
-    }
+      return todo;
+    });
 
-    function updateTodo(event, id) {
-        const updatedTodos = todos.map(todo => {
-            if (todo.id === id) {
-                if (event.target.value.trim().length === 0) {
-                    todo.isEditing = false;
-                    return todo;
-                }
-                todo.title = event.target.value;
-                todo.isEditing = false;
-            }
+    setTodos(updatedTodos);
+  }
 
-            return todo;
-        });
+  function cancelEdit(event, id) {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isEditing = false;
+      }
 
-        setTodos(updatedTodos);
-    }
+      return todo;
+    });
 
-    function cancelEdit(event, id) {
-        const updatedTodos = todos.map(todo => {
-            if (todo.id === id) {
-                todo.isEditing = false;
-            }
+    setTodos(updatedTodos);
+  }
 
-            return todo;
-        });
+  function completeAllTodos() {
+    const updatedTodos = todos.map(todo => {
+      todo.isComplete = true;
 
-        setTodos(updatedTodos);
-    }
+      return todo;
+    });
 
-    function completeAllTodos() {
-        const updatedTodos = todos.map(todo => {
-            todo.isComplete = true;
+    setTodos(updatedTodos);
+  }
 
-            return todo;
-        });
+  function remaining() {
+    return todos.filter(todo => !todo.isComplete).length;
+  }
 
-        setTodos(updatedTodos);
-    }
+  return (
+    <div>
+      <div>
+        <h2>Todo App</h2>
+        <TodoForm addTodo={addTodo} />
 
-    function remaining() {
-        return todos.filter(todo => !todo.isComplete).length;
-    }
-
-    return (
-        <div>
-            <div>
-                <h2>Todo App</h2>
-                <TodoForm addTodo={addTodo} />
-
-                {todos.length > 0 ? (
-                    <TodoList
-                        todos={todos}
-                        completeTodo={completeTodo}
-                        markAsEditing={markAsEditing}
-                        updateTodo={updateTodo}
-                        cancelEdit={cancelEdit}
-                        deleteTodo={deleteTodo}
-                        remaining={remaining}
-                        completeAllTodos={completeAllTodos}
-                    />
-                    ) : (
-                    <NoTodos />
-                )}
-            </div>
-        </div>
-    );
+        {todos.length > 0 ? (
+          <TodoList
+            todos={todos}
+            completeTodo={completeTodo}
+            markAsEditing={markAsEditing}
+            updateTodo={updateTodo}
+            cancelEdit={cancelEdit}
+            deleteTodo={deleteTodo}
+            remaining={remaining}
+            completeAllTodos={completeAllTodos}
+          />
+          ) : (
+          <NoTodos />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
