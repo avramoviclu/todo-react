ebcd261a674a1cf2ee074e7927b098523ac7306a
Luka Avramovic <avramoviclu@gmail.com>
2023-04-12 16:23:05 +0200

    Fixes className.

--- src/components/NoTodos.jsx
+++ src/components/NoTodos.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function NoTodos() {
   return (
-    <div class="alert alert-light" role="alert">
+    <div className="alert alert-light" role="alert">
       Add some todos...
     </div>
   );
